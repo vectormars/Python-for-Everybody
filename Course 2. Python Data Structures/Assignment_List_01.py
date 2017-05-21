bf6eddb4e21abc6f495c5509331451d4ad469a43
vectormars <xuejie328@gmail.com>
2017-05-20 22:45:04 -0400

    Add files via upload

--- Course 2. Python Data Structures/Assignment_List_01.py
+++ Course 2. Python Data Structures/Assignment_List_01.py
@@ -15,3 +15,10 @@ for line in fh:
               
 lst.sort()
 print lst
+
+
+"""
+Desired Output:
+    
+['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 'envious', 'fair', 'grief', 'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']
+"""
---
+++ Course 2. Python Data Structures/Assignment_List_02.py
@@ -0,0 +1,53 @@
+"""
+@author: jiexue
+"""
+
+fname = raw_input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    if not line.startswith("From:") : 
+        continue
+    words = line.rstrip().split()
+    count = count +1
+    print words[1]
+
+print "There were", count, "lines in the file with From as the first word"
+
+
+
+"""
+Desired Output:
+
+stephen.marquard@uct.ac.za
+louis@media.berkeley.edu
+zqian@umich.edu
+rjlowe@iupui.edu
+zqian@umich.edu
+rjlowe@iupui.edu
+cwen@iupui.edu
+cwen@iupui.edu
+gsilver@umich.edu
+gsilver@umich.edu
+zqian@umich.edu
+gsilver@umich.edu
+wagnermr@iupui.edu
+zqian@umich.edu
+antranig@caret.cam.ac.uk
+gopal.ramasammycook@gmail.com
+david.horwitz@uct.ac.za
+david.horwitz@uct.ac.za
+david.horwitz@uct.ac.za
+david.horwitz@uct.ac.za
+stephen.marquard@uct.ac.za
+louis@media.berkeley.edu
+louis@media.berkeley.edu
+ray@media.berkeley.edu
+cwen@iupui.edu
+cwen@iupui.edu
+cwen@iupui.edu
+There were 27 lines in the file with From as the first word
+"""
