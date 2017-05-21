be22869c1e81ec3058889075bc5307a56ee9693f
vectormars <xuejie328@gmail.com>
2017-05-20 22:25:35 -0400

    Add files via upload

---
+++ Course 2. Python Data Structures/Assignment_List_01.py
@@ -0,0 +1,17 @@
+"""
+@author: jiexue
+"""
+
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+lst = list()
+
+for line in fh:
+	words = line.rstrip().split()
+    
+        for word in words:
+            if word not in lst:
+                lst.append(word)
+              
+lst.sort()
+print lst
